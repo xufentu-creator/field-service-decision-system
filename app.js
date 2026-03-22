b89f45e9fbba02abbf88c3d25a240b43746f23e0
xufentu <246942485+xufentu-creator@users.noreply.github.com>
2026-03-22 15:23:49 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+let lang = "en";
+
+function setLang() {
+  const c = CONTENT[lang];
+
+  if (document.getElementById("title")) {
+    document.getElementById("title").innerText = c.home.title;
+    document.getElementById("subtitle").innerText = c.home.subtitle;
+    document.getElementById("startBtn").innerText = c.home.start;
+    document.getElementById("principle").innerText = c.home.principle;
+  }
+
+  if (document.getElementById("uploadTitle")) {
+    document.getElementById("uploadTitle").innerText = c.upload.title;
+    document.getElementById("generateBtn").innerText = c.upload.generate;
+  }
+
+  if (document.getElementById("resultTitle")) {
+    document.getElementById("resultTitle").innerText = c.result.title;
+    document.getElementById("current").innerText = c.result.current;
+    document.getElementById("optionA").innerText = c.result.optionA;
+    document.getElementById("optionB").innerText = c.result.optionB;
+    document.getElementById("optionC").innerText = c.result.optionC;
+    document.getElementById("boundary").innerText = c.result.boundary;
+  }
+}
+
+function goUpload() {
+  window.location.href = "upload.html";
+}
+
+function goResult() {
+  window.location.href = "result.html";
+}
+
+window.onload = setLang;
